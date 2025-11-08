7717a11e9178d77df70ee8d794ac239773420272
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-08 23:35:05 +0530

    feat : register emps in bulk and get all emps

--- src/main/java/com/jayesh/resourcePrj/controller/EmployeeController.java
+++ src/main/java/com/jayesh/resourcePrj/controller/EmployeeController.java
@@ -1,14 +1,33 @@
 package com.jayesh.resourcePrj.controller;
 
 
+import com.jayesh.resourcePrj.dto.response.EmployeeResponseDto;
+import com.jayesh.resourcePrj.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/emps")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
 
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponseDto>> findEmployees(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String username
+    ) {
+        List<EmployeeResponseDto> employees = employeeService.findAllEmployees(name,email,username);
+        return ResponseEntity.ok(employees);
+    }
 
 
 }
--- src/main/java/com/jayesh/resourcePrj/services/EmployeeService.java
+++ src/main/java/com/jayesh/resourcePrj/services/EmployeeService.java
@@ -7,12 +7,15 @@ import com.jayesh.resourcePrj.entities.Employee;
 import com.jayesh.resourcePrj.entities.Role;
 import com.jayesh.resourcePrj.repo.EmployeeRepo;
 import com.jayesh.resourcePrj.repo.RoleRepo;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,6 +28,21 @@ public class EmployeeService {
 
     public EmployeeResponseDto registerEmployee(EmployeeRequestDto request){
         log.info("Received request to register employee");
+        Employee employee = mapDtoToEmployee(request);
+        Employee saved = employeeRepo.save(employee);
+        log.info("Employee registered successfully");
+        return new EmployeeResponseDto(saved);
+    }
+    @Transactional
+    public List<EmployeeResponseDto> registerEmployeesInBulk(List<EmployeeRequestDto> request){
+        log.info("Received request to register employees in bulk");
+        List<Employee> employees = request.stream().map(this::mapDtoToEmployee).toList();
+        List<Employee> saved = employeeRepo.saveAll(employees);
+        log.info("All employees registered successfully");
+        return saved.stream().map(EmployeeResponseDto::new).toList();
+    }
+    public Employee mapDtoToEmployee(EmployeeRequestDto request){
+        log.info("Received request to map employee");
         if(employeeRepo.findEmployeeByUsername(request.getUsername()).isPresent()){
             throw new IllegalArgumentException("Employee already exists");
         }
@@ -37,8 +55,15 @@ public class EmployeeService {
         employee.setPassword(passwordEncoder.encode(request.getPassword()));
         employee.setUsername(request.getUsername());
         employee.setRoles(Set.of(role));
-        Employee saved = employeeRepo.save(employee);
-        log.info("Employee registered successfully");
-        return new EmployeeResponseDto(saved);
+        return employee;
     }
+
+    public List<EmployeeResponseDto> findAllEmployees(String name, String email, String username) {
+        log.info("Received request to find all employees");
+        List<Employee> employees = employeeRepo.findByCriteria(name, email, username);
+        log.info("Employees found successfully");
+        return employees.stream().map(EmployeeResponseDto::new).toList();
+    }
+
+
 }
