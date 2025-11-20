ea39128f9477c0f3f7ceb5b7bc6b0b264356d131
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-20 20:04:41 +0530

    feat : Login req resp dtos

---
+++ src/main/java/com/jayesh/resourcePrj/dto/response/LoginResponseDto.java
@@ -0,0 +1,22 @@
+package com.jayesh.resourcePrj.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class LoginResponseDto {
+
+
+    private String token;
+
+
+    private String username;
+
+
+    private String message;
+}
